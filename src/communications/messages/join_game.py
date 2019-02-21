f4e90b212b2ac5683c900ae3e3c11ba7fdba82f6
aaronmorgenegg <aaronmorgenegg@gmail.com>
2019-02-21 11:26:59 -0700

    Adding to message factory build method, created message type map

--- src/communications/messages/join_game.py
+++ src/communications/messages/join_game.py
@@ -5,5 +5,5 @@ class JoinGame(Request):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.game_id = args[1]
-        self.player_alias = args[2]
-        self.player_id = args[3]
+        self.player_id = args[2]
+        self.player_alias = args[3]

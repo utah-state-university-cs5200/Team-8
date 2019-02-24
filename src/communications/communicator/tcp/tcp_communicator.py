dea5906c8fbd0676b31de5d5822b6f7a9974d9cd
aaronmorgenegg <aaronmorgenegg@gmail.com>
2019-02-23 21:47:36 -0700

    Removing tcp communicator stub for now

--- src/communications/communicator/tcp/tcp_communicator.py
+++
@@ -1,9 +0,0 @@
-from src.communications.communicator.communicator import Communicator
-
-
-class TCPCommunicator(Communicator):
-    def __init__(self, group=None, target=None, name=None, *args, **kwargs):
-        super().__init__(group, target, name, args, kwargs)
-        self.socket = None
-        self.sender = None
-        self.receiver = None

43a1028fe49924b1548ae6da4840d412aa1152c7
aaronmorgenegg <aaronmorgenegg@gmail.com>
2019-03-03 20:56:46 -0700

    Socket timeouts, working on tcp comm tests

--- src/communications/communicator/tcp/tests/test_tcp_communicator.py
+++ src/communications/communicator/tcp/tests/test_tcp_communicator.py
@@ -1,4 +1,4 @@
-import time
+import socket
 import unittest
 from unittest.mock import Mock
 
@@ -11,29 +11,25 @@ from src.communications.messages.message_factory import MessageFactory
 class TestTCPCommunicator(unittest.TestCase):
 
     def setUp(self):
+        self.mock_server = Mock()
+        self.mock_server.alive = True
+        self.mock_server.addTCPConnection = lambda process_id, communicator: print(process_id, communicator)
+        self.listener = TCPListener(client=self.mock_server, address=("", 7774))
+        self.listener.start()
+
         self.mock_client = Mock()
         self.mock_client.alive = True
         self.mock_client.enqueueTask = lambda message: message
-        self.communicator = TCPCommunicator(self.mock_client, ("127.0.0.1", 7779))
-
-        self.mock_server = Mock()
-        self.mock_server.alive = True
-        self.mock_server.addTCPConnection = lambda process_id, communicator: (process_id, communicator)
-        self.listener = TCPListener(client=self.mock_server, address=("127.0.0.1", 7779))
-        self.listener.start()
-        # TODO: fix connection refused error
+        self.communicator = TCPCommunicator(self.mock_client, (socket.gethostname(), 7774))
 
     def tearDown(self):
         self.mock_client.alive = False
         self.mock_server.alive = False
         self.communicator.cleanup()
         self.listener.cleanup()
-        while self.communicator.sender.is_alive() or self.communicator.receiver.is_alive() or self.listener.is_alive():
-            print(self.communicator.sender.is_alive(), self.communicator.receiver.is_alive(), self.listener.is_alive())
-            time.sleep(1)
-        # TODO: figure out why sockets aren't getting closed. May need to use a resource manager('with' statement)
+        # TODO: fix socket unclosed error
 
     def testSendValidMessage(self):
         m1 = MessageFactory.build(message_type_id=MESSAGE_ID_HELLO, player_alias="Test Alias")
         self.communicator.sendMessage(m1)
-        # TODO: verify listener creates a new udp connection, and recieves the message
+        # TODO: verify listener creates a new connection, and recieves the message

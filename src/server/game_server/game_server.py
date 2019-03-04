bf932b42737c631ca498c05a054215d070a13b4c
aaronmorgenegg <aaronmorgenegg@gmail.com>
2019-03-03 19:27:58 -0700

    Allowing game server address to be set

--- src/server/game_server/game_server.py
+++ src/server/game_server/game_server.py
@@ -1,6 +1,7 @@
 from queue import Queue
 from threading import Thread
 
+from src.communications.communicator.constants import DEFAULT_GAME_SERVER_ADDRESS
 from src.communications.communicator.tcp.tcp_listener import TCPListener
 from src.communications.communicator.udp.udp_listener import UDPListener
 
@@ -9,11 +10,19 @@ class GameServer(Thread):
     def __init__(self, group=None, target=None, name=None, *args, **kwargs):
         super().__init__(group, target, name, args, kwargs)
         self.alive = True
-        self.tcp_listener = TCPListener(client=self)
-        self.udp_listener = UDPListener(client=self)
+        self._initAddress(kwargs)
+        self.tcp_listener = TCPListener(address=self.address, client=self)
+        self.udp_listener = UDPListener(address=self.address, client=self)
         self.tcp_comm_pool = {}
         self.udp_comm_pool = {}
         self.task_queue = Queue()
+        
+    def _initAddress(self, kwargs):
+        self.address = DEFAULT_GAME_SERVER_ADDRESS
+        try:
+            self.address = kwargs['address']
+        except KeyError:
+            pass
 
     def enqueueTask(self, task):
         self.task_queue.put(task)

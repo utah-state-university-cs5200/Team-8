b3a38e26973ecf19145cb35eba78d3da2ca7bd7c
aaronmorgenegg <aaronmorgenegg@gmail.com>
2019-03-27 22:55:51 -0600

    Refactoring player client inheritance structure

---
+++ src/client/client.py
@@ -0,0 +1,38 @@
+from threading import Thread
+
+from src.communications.communicator.tcp.tcp_communicator import TCPCommunicator
+from src.communications.communicator.udp.udp_communicator import UDPCommunicator
+from src.communications.conversation.conversation_dictionary import ConversationDictionary
+from src.communications.conversation.conversation_factory import ConversationFactory
+
+from src.server.constants import DEFAULT_LOBBY_UDP_ADDRESS, DEFAULT_LOBBY_TCP_ADDRESS
+from src.server.dispatcher import Dispatcher
+
+
+class Client(Thread):
+    def __init__(self, group=None, target=None, name=None, *args, **kwargs):
+        super().__init__(group, target, name, args, kwargs)
+        self._initServerAddresses()
+        self._initConversations()
+        self._initDispatcher()
+        self._initCommunicators()
+
+    def _initServerAddresses(self):
+        self.serverUDPAddress = DEFAULT_LOBBY_UDP_ADDRESS
+        self.serverTCPAddress = DEFAULT_LOBBY_TCP_ADDRESS
+
+    def _initConversations(self):
+        self.conversation_dict = ConversationDictionary()
+        self.conversation_factory = ConversationFactory()
+
+    def _initDispatcher(self):
+        self.dispatcher = Dispatcher(conversation_dict=self.conversation_dict,
+                                     conversation_factory=self.conversation_factory)
+
+    def _initCommunicators(self):
+        self.udp_communicator = UDPCommunicator(self.dispatcher, address=self.serverUDPAddress)
+        self.tcp_communicator = TCPCommunicator(self.dispatcher, address=self.serverTCPAddress)
+
+    def cleanup(self):
+        self.udp_communicator.cleanup()
+        self.tcp_communicator.cleanup()
--- src/client/player_client/player_client.py
+++ src/client/player_client/player_client.py
@@ -1,37 +1,6 @@
-from threading import Thread
-
-from src.communications.communicator.tcp.tcp_communicator import TCPCommunicator
-from src.communications.communicator.udp.udp_communicator import UDPCommunicator
-from src.communications.conversation.conversation_dictionary import ConversationDictionary
-from src.communications.conversation.conversation_factory import ConversationFactory
-from src.server.dispatcher import Dispatcher
+from src.client.client import Client
 
 
-class PlayerClient(Thread):
+class PlayerClient(Client):
     def __init__(self, group=None, target=None, name=None, *args, **kwargs):
         super().__init__(group, target, name, args, kwargs)
-        # TODO: should have some sort of UI object to get some of these values from the user
-        self._initServerAddresses()
-        self._initConversations()
-        self._initDispatcher()
-        self._initCommunicators()
-
-    def _initServerAddresses(self):
-        self.serverUDPAddress = ('127.0.0.1', 7777)
-        self.serverTCPAddress = ('127.0.0.1', 7778)
-
-    def _initConversations(self):
-        self.conversation_dict = ConversationDictionary()
-        self.conversation_factory = ConversationFactory()
-
-    def _initDispatcher(self):
-        self.dispatcher = Dispatcher(conversation_dict=self.conversation_dict,
-                                     conversation_factory=self.conversation_factory)
-
-    def _initCommunicators(self):
-        self.udp_communicator = UDPCommunicator(self.dispatcher, address=self.serverUDPAddress)
-        self.tcp_communicator = TCPCommunicator(self.dispatcher, address=self.serverTCPAddress)
-
-    def cleanup(self):
-        self.udp_communicator.cleanup()
-        self.tcp_communicator.cleanup()

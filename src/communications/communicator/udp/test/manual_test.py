a18944f40804730c4f16ddac31d01b5cd48fde65
aaronmorgenegg <aaronmorgenegg@gmail.com>
2019-03-29 18:07:53 -0600

    Fixing manual communicator test scripts

--- src/communications/communicator/udp/test/manual_test.py
+++ src/communications/communicator/udp/test/manual_test.py
@@ -8,18 +8,20 @@ from src.communications.messages.message_factory import MessageFactory
 
 class MockDispatcher:
     def processEnvelope(self, envelope):
-        print("Received {}".format(envelope))
+        print("Received {} from {}".format(envelope.message, envelope.address))
 
 mock_dispatcher = MockDispatcher()
 server_address = ('127.0.0.1', 7777)
 udp_listener = UDPListener(dispatcher=mock_dispatcher, address=server_address)
+udp_listener.start()
 
 udp_communicator = UDPCommunicator(dispatcher=mock_dispatcher, address=server_address)
 
 id_vals = {'message_id':2, 'sender_id':1}
 m1 = MessageFactory.build(message_type_id=MESSAGE_ID_HELLO, player_alias="Test Alias", **id_vals)
-print("Sending {}".format(m1))
+print("Sending {}".format(m1.getAttributes()))
 udp_communicator.sendMessage(m1)
 
 time.sleep(1)
-
+udp_communicator.cleanup()
+udp_listener.cleanup()

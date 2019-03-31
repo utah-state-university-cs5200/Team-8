17189efbaf64f659ca2ea6187b5a3d2f9002c59f
David Kunz <davidpkunz@gmail.com>
2019-03-31 15:53:02 -0600

    Start defining new client functions

--- src/client/player_client/player_client.py
+++ src/client/player_client/player_client.py
@@ -1,5 +1,6 @@
 from src.client.client import Client
 from src.contact.player import Player
+from src.communications.conversation.constants import *
 
 
 class PlayerClient(Client):
@@ -12,7 +13,31 @@ class PlayerClient(Client):
             conversation_type_id=HELLO_INITIATOR_CONVERSATION,
             conversation_id=self.getNextConversationID(),
             remote_endpoint=self.serverUDPAddress,
-            player_alias=self.player.alias,
-            timeout=timeout)
+            player_alias=self.player.alias
+            )
         hello_conversation.start()
         self.conversation_dict.add(hello_conversation)
+
+    def keeper_submit_word(self, word):
+        """Secret keeper submits a new word"""
+        pass
+
+    def keeper_block(self):
+        """Secret keeper blocks on a contact"""
+        pass
+
+    def keeper_call(self):
+        """Secret keeper calls on a contact"""
+        pass
+
+    def give_clue(self, clue):
+        """A player submits a clue for the word"""
+        pass
+
+    def initiate_contact(self):
+        """A player initiates contact on a clue"""
+        pass
+
+    def update_client(self):
+        """Updates all data/fields on the client"""
+        pass

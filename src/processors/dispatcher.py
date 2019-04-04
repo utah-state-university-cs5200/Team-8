a2783f81165e6c42bd22274f7c8376b9c0a5c611
aaronmorgenegg <aaronmorgenegg@gmail.com>
2019-04-03 18:24:21 -0600

    Fleshing out dispatcher

--- src/processors/dispatcher.py
+++ src/processors/dispatcher.py
@@ -1,3 +1,5 @@
+from src.communications.messages.encoder_decoder import decode
+
 
 class Dispatcher:
     def __init__(self, conversation_dict, conversation_factory):
@@ -5,5 +7,10 @@ class Dispatcher:
         self.conversation_factory = conversation_factory
 
     def process(self, envelope):
-        if len(envelope.message) != 0:
-            print("Received envelope from {} containing {}".format(envelope.address, envelope.message)) # TODO: process an envelope
+        conversation = self.conversation_dict.get(envelope.message.conversation_id)
+        if conversation is None:
+            message = decode(envelope.message)
+            # TODO: need to determine the conversation_type_id and pass that in
+            conversation = self.conversation_factory.build(remote_endpoint=envelope.address, **message.getAttributes())
+
+        conversation.process(envelope)

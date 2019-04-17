4e7845a6e7817c0ddb00e9cceaaad19d54762234
David Kunz <davidpkunz@gmail.com>
2019-04-17 01:08:14 -0600

    Scrollbar and Title updates

--- src/client/player_client/gui.py
+++ src/client/player_client/gui.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 
 class LobbyWindow:
+    """This class handles all UI elements for the lobby"""
 
     def __init__(self):
         self.listbox_options = {
@@ -16,24 +17,22 @@ class LobbyWindow:
                 'max_players': 8
             },
         }
-
-    def run(self):
-
         self.top = Tk()
-        self.top.title("Contact")
+        self.top.title("Contact Lobby")
         self.top.geometry("640x400")
 
+
+    def run(self):
         self.username()
         self.lobby_interface()
         self.update_listbox()
-
         self.top.mainloop()
 
     def username(self):
         """Enter username elements"""
 
         self.title_frame = Frame(self.top)
-        self.title_label = Label(self.title_frame, text="Contact")
+        self.title_label = Label(self.title_frame, text="Contact Lobby", font=('Helvetica', '24'))
         self.title_label.pack()
         self.title_frame.pack()
 
@@ -49,14 +48,20 @@ class LobbyWindow:
     def lobby_interface(self):
         """initialize the lobby elements"""
 
+
         self.create_game_button = Button(self.top, text="Create Game", command=self.create_game_init)
         self.create_game_button.pack()
 
-        self.lobby_listbox = Listbox(self.top, selectmode=SINGLE, width=100)
+        self.list_frame = Frame(self.top)
+        self.list_scrollbar = Scrollbar(self.list_frame)
+        self.list_scrollbar.pack(side=RIGHT, fill=Y)
+        self.lobby_listbox = Listbox(self.list_frame, selectmode=SINGLE, width=100, yscrollcommand=self.list_scrollbar.set)
         self.lobby_listbox.pack()
+
         self.lobby_listbox.bind('<<ListboxSelect>>', self.game_select)
+        self.list_scrollbar.config(command=self.lobby_listbox.yview)
 
-
+        self.list_frame.pack()
 
         self.player_count_label_var = StringVar()
         self.player_count_label_var.set("Players: ")
@@ -67,12 +72,8 @@ class LobbyWindow:
         self.join_game_button.pack()
         self.join_frame.pack()
 
-
-    def creation_interface(self):
-        """initialize the elements for game creation"""
-        pass
-
     def update_listbox(self):
+        """Update the game list"""
         count = 1
         self.lobby_listbox.delete(0, END)
         for key in self.listbox_options.keys():
@@ -81,16 +82,15 @@ class LobbyWindow:
 
 
     def username_change(self):
+        """Update the username"""
         self.username_entry.get()
         # TODO: update username in client code
 
-    def join_game(self):
-        game_name = self.lobby_listbox.get(self.lobby_listbox.curselection()[0])
-        game = self.listbox_options[game_name]
-        # TODO: call function to join game with game_id
 
 
     def create_game_init(self):
+        """Swap out widgets for game creation widgets"""
+        # TODO if the button is clicked multiple times it will keep adding things below, need to check if still there
         self.join_frame.pack_forget()
         self.create_game_frame = Frame(self.top)
         self.new_game_name_label = Label(self.create_game_frame, text="Game Name: ")
@@ -111,6 +111,7 @@ class LobbyWindow:
 
 
     def create_game(self):
+        """When Create Game button is pressed this is executed"""
         name = self.new_game_name_entry.get()
         max = self.new_game_players_entry.get()
         # TODO generate game_id or create the game server and have it return the game_id here
@@ -125,16 +126,48 @@ class LobbyWindow:
         self.update_listbox()
 
     def cancel_creation(self):
-
+        """Cancels game creation"""
         self.create_game_frame.pack_forget()
         self.join_frame.pack()
-        pass
 
     def game_select(self, e):
         game_name = self.lobby_listbox.get(self.lobby_listbox.curselection()[0])
         game = self.listbox_options[game_name]
 
         self.player_count_label_var.set(f"Players: {game['player_count']}/{game['max_players']}")
+
+    def join_game(self):
+        game_name = self.lobby_listbox.get(self.lobby_listbox.curselection()[0])
+        game = self.listbox_options[game_name]
+        # TODO: call function to join game with game_id
+
+
+class GamePlayWindow:
+
+    def __init__(self):
+        self.top = Tk()
+        self.top.title("Contact")
+        self.top.geometry("640x400")
+
+    def run(self):
+
+        self.top.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 test = LobbyWindow()
 test.run()

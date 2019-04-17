b48a597e31c843003c021fa8d07d3c278b3a03fe
David Kunz <davidpkunz@gmail.com>
2019-04-16 23:58:53 -0600

    Basic lobby GUI started.

---
+++ src/client/player_client/gui.py
@@ -0,0 +1,99 @@
+from tkinter import *
+
+
+class LobbyWindow:
+
+    def __init__(self):
+        self.listbox_options = {
+            'My Game': {
+                'game_id': 43,
+                'player_count': 3
+            },
+            'Another Game': {
+                'game_id': 23,
+                'player_count': 7
+            },
+        }
+
+    def run(self):
+
+        self.top = Tk()
+        self.top.title("Contact")
+        self.top.geometry("640x400")
+
+        self.username()
+        self.lobby_interface()
+        self.update_listbox()
+
+        self.top.mainloop()
+
+    def username(self):
+        """Enter username elements"""
+
+        self.title_frame = Frame(self.top)
+        self.title_label = Label(self.title_frame, text="Contact")
+        self.title_label.pack()
+        self.title_frame.pack()
+
+        self.username_frame = Frame(self.top)
+        self.username_label = Label(self.username_frame, text="Username: ")
+        self.username_entry = Entry(self.username_frame)
+        self.username_button = Button(self.username_frame, text="Change", command=self.username_change)
+        self.username_label.pack(side=LEFT)
+        self.username_entry.pack(side=LEFT)
+        self.username_button.pack(side=LEFT)
+        self.username_frame.pack(side=TOP)
+
+    def lobby_interface(self):
+        """initialize the lobby elements"""
+
+        self.lobby_listbox = Listbox(self.top, selectmode=SINGLE, width=100)
+        self.lobby_listbox.pack()
+        self.lobby_listbox.bind('<<ListboxSelect>>', self.game_select)
+
+        self.create_game_button = Button(self.top, text="Create Game", command=self.create_game_init)
+
+
+        self.player_count_label_var = StringVar()
+        self.player_count_label_var.set("Players: ")
+        self.join_frame = Frame(self.top)
+        self.player_count_label = Label(self.join_frame, textvariable = self.player_count_label_var)
+        self.join_game_button = Button(self.join_frame, text="Join Game", command=self.join_game)
+        self.player_count_label.pack()
+        self.join_game_button.pack()
+        self.join_frame.pack()
+
+
+    def creation_interface(self):
+        """initialize the elements for game creation"""
+        pass
+
+    def update_listbox(self):
+        count = 1
+        self.lobby_listbox.delete(1, END)
+        for key in self.listbox_options.keys():
+            self.lobby_listbox.insert(count, key)
+            count += 1
+
+
+    def username_change(self):
+        self.username_entry.get()
+        # TODO: update username in client code
+
+    def join_game(self):
+        game_name = self.lobby_listbox.get(self.lobby_listbox.curselection()[0])
+        game = self.listbox_options[game_name]
+        # TODO: call function to join game with game_id
+
+    def create_game_init(self):
+        pass
+
+
+    def game_select(self, e):
+        game_name = self.lobby_listbox.get(self.lobby_listbox.curselection()[0])
+        game = self.listbox_options[game_name]
+
+        self.player_count_label_var.set(f"Players: {game['player_count']}")
+
+test = LobbyWindow()
+test.run()
